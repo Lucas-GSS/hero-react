0f80099c42f35588364ea069aae1e16a93884017
Lucas Gabriel <lucasgabrieldesouza9@gmail.com>
2022-07-28 19:43:39 -0300

    BrBa

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Say my name.
         </p>
+        <p>You Heisenberg...</p>
+        <p><strong>You goddamn right!</strong></p>
         <a
           className="App-link"
           href="https://reactjs.org"
